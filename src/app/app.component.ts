d340e880ecb0be2a9a1cb710313d5211f51b56b0
pratik <pratikdesai2274@gmail.com>
2023-10-21 12:49:16 +0530

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,47 +6,71 @@ import { FormGroup , FormControl , Validator, Validators } from '@angular/forms'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'form';
-  mobilnumber = /(\d{3})(\d{3})(\d{4})/ ;
-  letterpatturn = /^[a-z,A-Z]/ ;
+  mobilnumber = /^[0-9]{10}$/ ;
+  letterpatturn = /^[a-zA-Z]{2,}$/ ;
   emailvalidatin =  /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
 
   registrationForm = new FormGroup ({ 
-    name : new FormControl('', [Validators.required , Validators.pattern(this.letterpatturn) ]),
-    // useremail : new FormControl('', [Validators.required , Validators.email]),
+    name : new FormControl('', [Validators.required , Validators.pattern(this.letterpatturn)]),
     useremail : new FormControl('', [Validators.required , Validators.pattern(this.emailvalidatin)]),
-    password : new FormControl('', [Validators.required , Validators.minLength(6)]), 
-    CunfurmPassword : new FormControl('', [Validators.required , Validators.minLength(6)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    CunfurmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
     age : new FormControl('', [Validators.required , Validators.min(18) , Validators.max(99)]),
     mobile : new FormControl('', [Validators.required , Validators.pattern(this.mobilnumber)])
   })
-
-  registrationUser(){
-   console.log(this.registrationForm.value); 
+  
+ 
+  data: any[] = []; 
+  registrationUser() {
+    if (this.registrationForm.valid && this.passwordsDoNotMatch()) {
+          const newData = this.registrationForm.value;
+          this.data.push(newData);
+          console.log(this.data); 
+          this.resetForm();
+      } else {
+        console.log('Form is not valid or passwords do not match');
+      }
   } 
-   controll(www:string){
+  
+  // password hide show
+  password :string = "password"; 
+  show = true; 
+  onClick() {
+    if (this.password === 'password') {
+      this.password = 'text';
+      this.show = false;
+    } else {
+      this.password = 'password';
+      this.show = true;
+    }
+  } 
+  Confpassword :string ="password" ; 
+  Confshow = true; 
+  aaa() {
+    if (this.Confpassword === 'password') {
+      this.Confpassword = 'text';
+      this.Confshow = false;
+    } else {
+      this.Confpassword = 'password';
+      this.Confshow = true;
+    }
+  }
+ 
+  resetForm() {
+    this.registrationForm.reset(); 
+  }
+  passwordsDoNotMatch() {
+    const password = this.registrationForm.get('password')?.value;
+    const confirmPassword = this.registrationForm.get('CunfurmPassword')?.value;
+    return password == confirmPassword;
+  }
+  controll(www: string) {
     return this.registrationForm.get(www) &&
-    this.registrationForm.get(www)?.invalid &&
-    this.registrationForm.get(www)?.touched
+      this.registrationForm.get(www)?.invalid &&
+      this.registrationForm.get(www)?.touched;
   } 
-
-  // get useremail() {
-  //   return this.registrationForm.get('useremail')
-  // } 
-  // get name() {
-  //   return this.registrationForm.get('name')
-  // }
-  // get password() {
-  //   return this.registrationForm.get('password')
-  // }
-  // get CunfurmPassword() {
-  //   return this.registrationForm.get('CunfurmPassword')
-  // }
-  // get age() {
-  //   return this.registrationForm.get('age')
-  // }
-  // get mobile() {
-  //   return this.registrationForm.get('mobile')
-  // }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule , FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component'; 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    FormsModule, 
-    ReactiveFormsModule
-  ],
+  declarations: [AppComponent,HomeComponent],
+  imports: [ BrowserModule,  AppRoutingModule, NgbModule, FormsModule, ReactiveFormsModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
